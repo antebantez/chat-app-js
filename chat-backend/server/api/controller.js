f31f86b7927bedc482f01fbdbe041618f8959bbd
Anton Olsson <antonolsson.imp@gmail.com>
2023-01-03 11:31:13 +0100

    Initial commit

---
+++ chat-backend/server/api/controller.js
@@ -0,0 +1,172 @@
+require('./db');
+
+const createUser = async (req, res) => {
+    if (!req.) {
+        res.status(500).json({ success: false, error: 'Incorrect parameters' });
+    }
+
+    try {
+
+    }
+    catch (err) {
+        res.status(500).json({ success: false, error: err.message });
+    }
+}
+
+const loginUser = async (req, res) => {
+    if (!req.) {
+        res.status(500).json({ success: false, error: 'Incorrect parameters' });
+    }
+
+    try {
+
+    }
+    catch (err) {
+        res.status(500).json({ success: false, error: err.message });
+    }
+}
+
+const blockUser = async (req, res) => {
+    if (!req.) {
+        res.status(500).json({ success: false, error: 'Incorrect parameters' });
+    }
+
+    try {
+
+    }
+    catch (err) {
+        res.status(500).json({ success: false, error: err.message });
+    }
+}
+
+const getChats = async (req, res) => {
+    if (!req.) {
+        res.status(500).json({ success: false, error: 'Incorrect parameters' });
+    }
+
+    try {
+
+    }
+    catch (err) {
+        res.status(500).json({ success: false, error: err.message });
+    }
+}
+
+const createChat = async (req, res) => {
+    if (!req.) {
+        res.status(500).json({ success: false, error: 'Incorrect parameters' });
+    }
+
+    try {
+
+    }
+    catch (err) {
+        res.status(500).json({ success: false, error: err.message });
+    }
+}
+
+const inviteToChat = async (req, res) => {
+    if (!req.) {
+        res.status(500).json({ success: false, error: 'Incorrect parameters' });
+    }
+
+    try {
+
+    }
+    catch (err) {
+        res.status(500).json({ success: false, error: err.message });
+    }
+}
+
+const acceptChatInvite = async (req, res) => {
+    if (!req.) {
+        res.status(500).json({ success: false, error: 'Incorrect parameters' });
+    }
+
+    try {
+
+    }
+    catch (err) {
+        res.status(500).json({ success: false, error: err.message });
+    }
+}
+
+const banFromChat = async (req, res) => {
+    if (!req.) {
+        res.status(500).json({ success: false, error: 'Incorrect parameters' });
+    }
+
+    try {
+
+    }
+    catch (err) {
+        res.status(500).json({ success: false, error: err.message });
+    }
+}
+
+
+const sendMessage = async (req, res) => {
+    if (!req.) {
+        res.status(500).json({ success: false, error: 'Incorrect parameters' });
+    }
+
+    try {
+
+    }
+    catch (err) {
+        res.status(500).json({ success: false, error: err.message });
+    }
+}
+
+const getChatMessages = async (req, res) => {
+    if (!req.) {
+        res.status(500).json({ success: false, error: 'Incorrect parameters' });
+    }
+
+    try {
+
+    }
+    catch (err) {
+        res.status(500).json({ success: false, error: err.message });
+    }
+}
+
+const deleteMessage = async (req, res) => {
+    if (!req.) {
+        res.status(500).json({ success: false, error: 'Incorrect parameters' });
+    }
+
+    try {
+
+    }
+    catch (err) {
+        res.status(500).json({ success: false, error: err.message });
+    }
+}
+
+/* const x = async (req, res) => {
+    if (!req.) {
+        res.status(500).json({ success: false, error: 'Incorrect parameters' });
+    }
+
+    try {
+
+    }
+    catch (err) {
+        res.status(500).json({ success: false, error: err.message });
+    }
+} */
+
+module.exports = {
+    createUser,
+    loginUser,
+    blockUser,
+    getChats,
+    createChat,
+    inviteToChat,
+    acceptChatInvite,
+    banFromChat,
+    sendMessage,
+    getChatMessages,
+    deleteMessage
+}
